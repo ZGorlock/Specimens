4edc3e17635ed386b7a095037b6d0f2c53093699
Zachary Gill <ZGorlock@gmail.com>
2020-02-21 22:46:16 -0500

    Initial commit

---
+++ specimens/0001/scripts/imagePopup.js
@@ -0,0 +1,179 @@
+$(document).ready(function() {
+	var scroll = 0;
+
+	$(document).on('scroll', 'html, body', function() {
+		scroll = $(window).scrollTop();
+	});
+
+	function prepareShow() {
+		scroll = $(window).scrollTop();
+		$('html, body').css({
+			overflow: 'hidden',
+			height: '100%'
+		});
+	}
+	function prepareHide() {
+		$('html, body').css({
+			overflow: 'auto',
+			height: 'auto'
+		});
+		$("html").scrollTop(scroll);
+	}
+
+	$('#img_preliminary_attempt0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_preliminary_attempt0").show();
+	});
+	$('#div_img_preliminary_attempt0').click(function(){
+		prepareHide();
+	$("#div_img_preliminary_attempt0").hide();
+	});
+	$('#div_close_img_preliminary_attempt0').click(function(){
+		prepareHide();
+	$("#div_img_preliminary_attempt0").hide();
+	});
+
+	$('#img_preparation0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_preparation0").show();
+	});
+	$('#div_img_preparation0').click(function(){
+		prepareHide();
+	$("#div_img_preparation0").hide();
+	});
+	$('#div_close_img_preparation0').click(function(){
+		prepareHide();
+	$("#div_img_preparation0").hide();
+	});
+
+	$('#img_preparation1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_preparation1").show();
+	});
+	$('#div_img_preparation1').click(function(){
+		prepareHide();
+	$("#div_img_preparation1").hide();
+	});
+	$('#div_close_img_preparation1').click(function(){
+		prepareHide();
+	$("#div_img_preparation1").hide();
+	});
+
+	$('#img_preparation2').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_preparation2").show();
+	});
+	$('#div_img_preparation2').click(function(){
+		prepareHide();
+	$("#div_img_preparation2").hide();
+	});
+	$('#div_close_img_preparation2').click(function(){
+		prepareHide();
+	$("#div_img_preparation2").hide();
+	});
+
+	$('#img_suspension0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_suspension0").show();
+	});
+	$('#div_img_suspension0').click(function(){
+		prepareHide();
+	$("#div_img_suspension0").hide();
+	});
+	$('#div_close_img_suspension0').click(function(){
+		prepareHide();
+	$("#div_img_suspension0").hide();
+	});
+
+	$('#img_suspension1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_suspension1").show();
+	});
+	$('#div_img_suspension1').click(function(){
+		prepareHide();
+	$("#div_img_suspension1").hide();
+	});
+	$('#div_close_img_suspension1').click(function(){
+		prepareHide();
+	$("#div_img_suspension1").hide();
+	});
+
+	$('#img_suspension2').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_suspension2").show();
+	});
+	$('#div_img_suspension2').click(function(){
+		prepareHide();
+	$("#div_img_suspension2").hide();
+	});
+	$('#div_close_img_suspension2').click(function(){
+		prepareHide();
+	$("#div_img_suspension2").hide();
+	});
+
+	$('#img_suspension3').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_suspension3").show();
+	});
+	$('#div_img_suspension3').click(function(){
+		prepareHide();
+	$("#div_img_suspension3").hide();
+	});
+	$('#div_close_img_suspension3').click(function(){
+		prepareHide();
+	$("#div_img_suspension3").hide();
+	});
+
+	$('#img_suspension4').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_suspension4").show();
+	});
+	$('#div_img_suspension4').click(function(){
+		prepareHide();
+	$("#div_img_suspension4").hide();
+	});
+	$('#div_close_img_suspension4').click(function(){
+		prepareHide();
+	$("#div_img_suspension4").hide();
+	});
+
+	$('#img_suspension5').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_suspension5").show();
+	});
+	$('#div_img_suspension5').click(function(){
+		prepareHide();
+	$("#div_img_suspension5").hide();
+	});
+	$('#div_close_img_suspension5').click(function(){
+		prepareHide();
+	$("#div_img_suspension5").hide();
+	});
+
+	$(document).keyup(function(e) {
+		if (e.key === "Escape") {
+			prepareHide();
+			$("#div_img_preliminary_attempt0").hide();
+			$("#div_img_preparation0").hide();
+			$("#div_img_preparation1").hide();
+			$("#div_img_preparation2").hide();
+			$("#div_img_suspension0").hide();
+			$("#div_img_suspension1").hide();
+			$("#div_img_suspension2").hide();
+			$("#div_img_suspension3").hide();
+			$("#div_img_suspension4").hide();
+			$("#div_img_suspension5").hide();
+		}
+	});
+
+});
