2ff37e2b489b0cd4038bfa25447cfd3fee028724
Zachary Gill <ZGorlock@gmail.com>
2020-03-06 19:21:29 -0500

    Saved states of togglers in navbar and tree view

---
+++ scripts/treeViewToggler.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	var toggler = document.getElementsByClassName("caret");
+
+	var i;
+	for (i = 0; i < toggler.length; i++) {
+
+		toggler[i].setAttribute('id', i);
+		if (sessionStorage.getItem('treeViewCollapsed' + i) === "true") {
+			toggler[i].parentElement.querySelector(".nested").classList.toggle("active");
+			toggler[i].classList.toggle("caret-down");
+		}
+
+		toggler[i].addEventListener("click", function() {
+			this.parentElement.querySelector(".nested").classList.toggle("active");
+			this.classList.toggle("caret-down");
+
+			if (sessionStorage.getItem('treeViewCollapsed' + this.getAttribute('id')) == "true") {
+				sessionStorage.setItem('treeViewCollapsed' + this.getAttribute('id'), false);
+			} else {
+				sessionStorage.setItem('treeViewCollapsed' + this.getAttribute('id'), true);
+			}
+
+		});
+	}
+});
