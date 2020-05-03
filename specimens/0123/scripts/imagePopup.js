744e92337e1f6ddd53a1cee3bc198484d70929f7
Zachary Gill <ZGorlock@gmail.com>
2020-05-02 23:29:13 -0400

    Finalized old Specimens

--- specimens/0123/scripts/imagePopup.js
+++ specimens/0123/scripts/imagePopup.js
@@ -118,6 +118,34 @@ $(document).ready(function() {
 	$("#div_img_preparation1").hide();
 	});
 
+	$('#img_final0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final0").show();
+	});
+	$('#div_img_final0').click(function(){
+		prepareHide();
+	$("#div_img_final0").hide();
+	});
+	$('#div_close_img_final0').click(function(){
+		prepareHide();
+	$("#div_img_final0").hide();
+	});
+
+	$('#img_final1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final1").show();
+	});
+	$('#div_img_final1').click(function(){
+		prepareHide();
+	$("#div_img_final1").hide();
+	});
+	$('#div_close_img_final1').click(function(){
+		prepareHide();
+	$("#div_img_final1").hide();
+	});
+
 	$(document).keyup(function(e) {
 		if (e.key === "Escape") {
 			prepareHide();
@@ -128,6 +156,8 @@ $(document).ready(function() {
 			$("#div_img_dead1").hide();
 			$("#div_img_preparation0").hide();
 			$("#div_img_preparation1").hide();
+			$("#div_img_final0").hide();
+			$("#div_img_final1").hide();
 		}
 	});
 
