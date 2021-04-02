0fcad5147b524b8b6de6460d219e531f78e91ac4
Zachary Gill <zachary.w.gill@gmail.com>
2021-04-02 15:20:09 -0400

    Added new Specimens

---
+++ specimens/0253/scripts/imagePopup.js
@@ -0,0 +1,134 @@
+$(document).ready(function() {
+	var scroll = 0;
+
+	$(document).on('scroll', 'html, body', function() {
+		scroll = $(window).scrollTop();
+	});
+
+	function prepareShow() {
+		scroll = $(window).scrollTop();
+		$('html, body').css({
+			overflow: 'hidden',
+			height: '100%'
+		});
+	}
+	function prepareHide() {
+		$('html, body').css({
+			overflow: 'auto',
+			height: 'auto'
+		});
+		$("html").scrollTop(scroll);
+	}
+
+	$('#img_alive0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_alive0").show();
+	});
+	$('#div_img_alive0').click(function(){
+		prepareHide();
+		$("#div_img_alive0").hide();
+	});
+	$('#div_close_img_alive0').click(function(){
+		prepareHide();
+		$("#div_img_alive0").hide();
+	});
+
+	$('#img_alive1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_alive1").show();
+	});
+	$('#div_img_alive1').click(function(){
+		prepareHide();
+		$("#div_img_alive1").hide();
+	});
+	$('#div_close_img_alive1').click(function(){
+		prepareHide();
+		$("#div_img_alive1").hide();
+	});
+
+	$('#img_alive2').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_alive2").show();
+	});
+	$('#div_img_alive2').click(function(){
+		prepareHide();
+		$("#div_img_alive2").hide();
+	});
+	$('#div_close_img_alive2').click(function(){
+		prepareHide();
+		$("#div_img_alive2").hide();
+	});
+
+	$('#img_dead0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_dead0").show();
+	});
+	$('#div_img_dead0').click(function(){
+		prepareHide();
+		$("#div_img_dead0").hide();
+	});
+	$('#div_close_img_dead0').click(function(){
+		prepareHide();
+		$("#div_img_dead0").hide();
+	});
+
+	$('#img_dead1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_dead1").show();
+	});
+	$('#div_img_dead1').click(function(){
+		prepareHide();
+		$("#div_img_dead1").hide();
+	});
+	$('#div_close_img_dead1').click(function(){
+		prepareHide();
+		$("#div_img_dead1").hide();
+	});
+
+	$('#img_dead2').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_dead2").show();
+	});
+	$('#div_img_dead2').click(function(){
+		prepareHide();
+		$("#div_img_dead2").hide();
+	});
+	$('#div_close_img_dead2').click(function(){
+		prepareHide();
+		$("#div_img_dead2").hide();
+	});
+
+	$('#img_preparation0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_preparation0").show();
+	});
+	$('#div_img_preparation0').click(function(){
+		prepareHide();
+		$("#div_img_preparation0").hide();
+	});
+	$('#div_close_img_preparation0').click(function(){
+		prepareHide();
+		$("#div_img_preparation0").hide();
+	});
+
+	$(document).keyup(function(e) {
+		if (e.key === "Escape") {
+			prepareHide();
+			$("#div_img_alive0").hide();
+			$("#div_img_alive1").hide();
+			$("#div_img_alive2").hide();
+			$("#div_img_dead0").hide();
+			$("#div_img_dead1").hide();
+			$("#div_img_dead2").hide();
+			$("#div_img_preparation0").hide();
+		}
+	});
+
+});
