380baf9dd931b764a556bef15d3d64a2dbc3d0bb
Zachary Gill <zachary.w.gill@gmail.com>
2021-05-24 20:47:19 -0400

    Finalized Specimens

--- specimens/0238/scripts/imagePopup.js
+++ specimens/0238/scripts/imagePopup.js
@@ -104,6 +104,34 @@ $(document).ready(function() {
 		$("#div_img_exhumation1").hide();
 	});
 
+	$('#img_final0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final0").show();
+	});
+	$('#div_img_final0').click(function(){
+		prepareHide();
+		$("#div_img_final0").hide();
+	});
+	$('#div_close_img_final0').click(function(){
+		prepareHide();
+		$("#div_img_final0").hide();
+	});
+
+	$('#img_final1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final1").show();
+	});
+	$('#div_img_final1').click(function(){
+		prepareHide();
+		$("#div_img_final1").hide();
+	});
+	$('#div_close_img_final1').click(function(){
+		prepareHide();
+		$("#div_img_final1").hide();
+	});
+
 	$(document).keyup(function(e) {
 		if (e.key === "Escape") {
 			prepareHide();
@@ -113,6 +141,8 @@ $(document).ready(function() {
 			$("#div_img_preparation0").hide();
 			$("#div_img_exhumation0").hide();
 			$("#div_img_exhumation1").hide();
+			$("#div_img_final0").hide();
+			$("#div_img_final1").hide();
 		}
 	});
 
