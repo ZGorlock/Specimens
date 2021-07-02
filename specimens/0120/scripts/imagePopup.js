e4f9e8b96e92e0bec5b4c9bd70c87cf06997423f
Zachary Gill <zachary.w.gill@gmail.com>
2021-07-01 22:45:03 -0400

    Finalized old Specimens

--- specimens/0120/scripts/imagePopup.js
+++ specimens/0120/scripts/imagePopup.js
@@ -244,6 +244,62 @@ $(document).ready(function() {
 		$("#div_img_preparation4").hide();
 	});
 
+	$('#img_final0').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final0").show();
+	});
+	$('#div_img_final0').click(function(){
+		prepareHide();
+		$("#div_img_final0").hide();
+	});
+	$('#div_close_img_final0').click(function(){
+		prepareHide();
+		$("#div_img_final0").hide();
+	});
+
+	$('#img_final1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final1").show();
+	});
+	$('#div_img_final1').click(function(){
+		prepareHide();
+		$("#div_img_final1").hide();
+	});
+	$('#div_close_img_final1').click(function(){
+		prepareHide();
+		$("#div_img_final1").hide();
+	});
+
+	$('#img_final2').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final2").show();
+	});
+	$('#div_img_final2').click(function(){
+		prepareHide();
+		$("#div_img_final2").hide();
+	});
+	$('#div_close_img_final2').click(function(){
+		prepareHide();
+		$("#div_img_final2").hide();
+	});
+
+	$('#img_final3').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_final3").show();
+	});
+	$('#div_img_final3').click(function(){
+		prepareHide();
+		$("#div_img_final3").hide();
+	});
+	$('#div_close_img_final3').click(function(){
+		prepareHide();
+		$("#div_img_final3").hide();
+	});
+
 	$(document).keyup(function(e) {
 		if (e.key === "Escape") {
 			prepareHide();
@@ -263,6 +319,10 @@ $(document).ready(function() {
 			$("#div_img_preparation2").hide();
 			$("#div_img_preparation3").hide();
 			$("#div_img_preparation4").hide();
+			$("#div_img_final0").hide();
+			$("#div_img_final1").hide();
+			$("#div_img_final2").hide();
+			$("#div_img_final3").hide();
 		}
 	});
 
