cd61796929d179a860c196c0224c3ba847c7b794
Zachary Gill <zachary.w.gill@gmail.com>
2022-03-23 23:52:19 -0400

    Updated Specimen

--- specimens/0401/scripts/imagePopup.js
+++ specimens/0401/scripts/imagePopup.js
@@ -104,6 +104,20 @@ $(document).ready(function() {
 		$("#div_img_preparation0").hide();
 	});
 
+	$('#img_preparation1').click( function(e) {
+		e.preventDefault();
+		prepareShow();
+		$("#div_img_preparation1").show();
+	});
+	$('#div_img_preparation1').click(function(){
+		prepareHide();
+		$("#div_img_preparation1").hide();
+	});
+	$('#div_close_img_preparation1').click(function(){
+		prepareHide();
+		$("#div_img_preparation1").hide();
+	});
+
 	$(document).keyup(function(e) {
 		if (e.key === "Escape") {
 			prepareHide();
@@ -113,6 +127,7 @@ $(document).ready(function() {
 			$("#div_img_dead3").hide();
 			$("#div_img_dead4").hide();
 			$("#div_img_preparation0").hide();
+			$("#div_img_preparation1").hide();
 		}
 	});
 
